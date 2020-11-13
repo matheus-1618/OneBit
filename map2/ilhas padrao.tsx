faa18abb552ecb3cd85301f58c8af4a747dc13e8
matheus-1618 <matheus1207live@gmail.com>
2020-11-13 00:59:52 -0300

    Ilhas

---
+++ map2/ilhas padrao.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="ilhas padrao" tilewidth="32" tileheight="32" tilecount="629" columns="17">
+ <image source="../img/tileset.png" width="544" height="1200"/>
+</tileset>
---
+++ map2/terreno_final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="terreno_final" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../img/kisspng-sprite-tile-based-video-game-opengameart-org-tiles-5ad7e1d7a1c119.0323451215240974956626.png" width="960" height="960"/>
+</tileset>
